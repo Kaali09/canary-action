12ec627baf24a1bc97db45079b03c5a661332412
kaali <kaliraja1992@gmail.com>
2021-09-30 17:40:15 +0530

    updated the action WF

--- scripts/bounce.sh
+++ scripts/bounce.sh
@@ -4,7 +4,7 @@ set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 mkdir -p ~/.kube
-echo $kube_config | base64 -d > ~/.kube/config
+echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 
 # Execute kubectl command
-kubectl get po -n $1
+kubectl get po -n $INPUT_namespace
