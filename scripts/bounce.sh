e1b3f31a8c34196b4e5b15ef2034116909ecc178
kaali <kaliraja1992@gmail.com>
2021-09-30 18:24:38 +0530

    added the args

--- scripts/bounce.sh
+++ scripts/bounce.sh
@@ -5,10 +5,10 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
-
+kubectl get ns
 # Execute kubectl command
-ns=`kubectl get ns | grep confluent | xargs`
-for i in $ns; do
-echo "Listing pods in namespace: $i"
-kubectl get po -n "$i"
-done
+#ns=`kubectl get ns | grep confluent | xargs`
+#for i in $ns; do
+#echo "Listing pods in namespace: $i"
+#kubectl get po -n "$i"
+#done
