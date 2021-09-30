5ae5fe582479c4bfbe994c24e43e5284d95f0b01
kaali <kaliraja1992@gmail.com>
2021-09-30 19:10:00 +0530

    added the args

--- scripts/bounce.sh
+++ scripts/bounce.sh
@@ -7,8 +7,12 @@ mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 kubectl get ns
 # Execute kubectl command
+if [ $# -eq 0 ]; then
 ns=`kubectl get ns | grep kube | awk '{print $1}'`
 for i in $ns; do
 echo "Listing pods in namespace: $i"
 kubectl get po -n "$i"
 done
+
+else
+kubectl get po -n $*
