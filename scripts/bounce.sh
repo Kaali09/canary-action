77b2a6e13331781ee2cce7669c7be4f1c9fb069c
kaali <kaliraja1992@gmail.com>
2021-10-01 11:05:27 +0530

    added the args

--- scripts/bounce.sh
+++ scripts/bounce.sh
@@ -5,21 +5,27 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
-# Execute kubectl command
+
+
 if [ $# -eq 0 ]; then
-ns=`kubectl get ns | grep kube | awk '{print $1}'`
-for i in $ns; do
-echo "Listing pods in namespace: $i"
-kubectl get po -n "$i"
-done
+   ns=`kubectl get ns | grep confluent | awk '{print $1}'`
+      for i in $ns; do
+      echo "scaling down the deployment of $i"
+         kubectl scale deployment nginx-deployment --replicas=0 -n $i
+      done
+
+      sleep 20
+
+      for i in $ns; do
+      echo "Scaling up the deployment of $i"
+         kubectl scale deployment nginx-deployment --replicas=1 -n $i
+      done
 
 else
-kubectl get po -n $1
-echo "scaling down the deployment"
-kubectl scale deployment nginx-deployment --replicas=0 -n $1
+      echo "scaling down the deployment"
+         kubectl scale deployment nginx-deployment --replicas=0 -n $1
 
-sleep 10
-echo "Scaling up the deployment"
-kubectl scale deployment nginx-deployment --replicas=1 -n $1
-
+      sleep 20
+      echo "Scaling up the deployment"
+         kubectl scale deployment nginx-deployment --replicas=1 -n $1
 fi
